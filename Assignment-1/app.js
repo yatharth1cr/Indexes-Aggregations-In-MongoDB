2f4d97c12fc71be30cf6a99919c28ed800901c0f
yatharth1cr <yatharthgiri187@gmail.com>
2024-08-12 23:45:40 +0530

    completed few parts of assignment

---
+++ Assignment-1/app.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+var path = require("path"); // Added this line
+
+// connect mongoDB
+mongoose
+  .connect("mongodb://localhost/user-article", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected true");
+  })
+  .catch((err) => {
+    console.log("Connection error: ", err);
+  });
+
+// instantiate the app
+var app = express();
+
+// middlewares
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// routes
+app.get("/", (req, res, next) => {
+  res.send("Welcome to Express App");
+});
+
+// listener
+app.listen(4000, () => {
+  console.log("Server is listening at 4k");
+});
